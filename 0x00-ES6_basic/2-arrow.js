ae53ff80933eb37edc1af5dd5aabb0739fe8b843
JaysonSey <lytlebhanks44@gmail.com>
2024-04-12 17:19:15 +0000

    2-arrow.js

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,10 +1,11 @@
-const getNeighborhoodsList = () => {
-  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+export default class getNeighborhoodsList {
+  constructor() {
+    this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+    this.addNeighborhood = this.addNeighborhood.bind(this);
+  }
 
-  this.addNeighborhood = (newNeighborhood) => {
+  addNeighborhood(newNeighborhood) {
     this.sanFranciscoNeighborhoods.push(newNeighborhood);
     return this.sanFranciscoNeighborhoods;
-  };
-};
-
-export default getNeighborhoodsList;
+  }
+}
