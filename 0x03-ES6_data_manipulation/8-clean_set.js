be9e5959e783d29c4906bd411799a67180f7b25e
JaysonSey <lytlebhanks44@gmail.com>
2024-04-16 18:30:08 +0000

    8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,10 +2,10 @@ function cleanSet(set, startString) {
   let result = '';
   set.forEach((value) => {
     if (value.startsWith(startString)) {
-      result += (result.length > 0 ? '-' : '') + value.substring(startString.length);
+      result += `${value.substring(startString.length)}-`;
     }
   });
-  return result;
+  return result.slice(0, -1);
 }
 
 export default cleanSet;
